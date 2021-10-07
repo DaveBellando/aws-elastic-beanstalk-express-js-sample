91eafc0647aeae825d22bff02ef34a2e246687c1
Dave B <dave.bellando@credera.com>
2021-10-07 15:12:07 -0500

    Update for Product B

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Dave Bellando from Visual Studio! This demonstrates the Review before deployment. V2'));
+app.get('/', (req, res) => res.send('Hello World from Dave Bellando from Visual Studio! This is Product B'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
